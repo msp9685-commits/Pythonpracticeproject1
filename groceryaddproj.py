f67de4b80ce54c943d5bded6a8011794d514f191
msp9685-commits <msp9685@gmail.com>
2026-04-22 23:31:38 +0530

    Add files via upload
    addition calculator for grocery shop

---
+++ groceryaddproj.py
@@ -0,0 +1,12 @@
+sum = 0
+while True:
+    num1 = int(input("enter a number or press '0' to exit: "))
+    sum += num1
+    print(sum)
+
+
+    if num1 == 0:
+        sum = 0
+        print(sum)
+
+        '''push code to list and add to file and print '''
